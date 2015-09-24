58126de089e08ec426514a16e9321c93a26bccd8
Senthilkumar Manoharan <smanoharan@C02N22YWFD56.local>
2015-09-24 08:41:31 -0700

    modified for mobile and video tag

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,4 @@
-    var clrIntrvl;
+    var clrIntrvl,videoRm = false;
     $(document).ready(function(){
     
         window.setTimeout(function(){$('.vinodhaImg,.anandImg').addClass("opImg");},1000);
@@ -16,13 +16,14 @@
             
             $('footer.row.desktopMenu ul li').removeClass("current read");
             var menus = $(this).find('a').attr("href");
+            menus = menus.replace("#","");
             fnLoadMenus(menus);
             $(this).addClass("current");
             $('footer.row.desktopMenu ul li').find("i").removeClass("fa-heart");
             $('footer.row.desktopMenu ul li').find("i").addClass("fa-heart-o");
             $(this).find("i").addClass("fa-heart");
             $(this).find("i").removeClass("fa-heart-o");
-            fnLoadMenus();
+            
             e.stopPropagation();
         });
         
@@ -40,7 +41,21 @@
            }
            
         });
-           
+        
+        
+        $('footer.row.mobileMenu i ,footer.row.mobileMenu .mobileCurrMenu ').click(function(e){
+            
+            $('footer.mobileMenu').addClass("menuOpen");
+        });
+        
+        $('footer.row.mobileMenu li ').click(function(e){
+            var menus = $(this).find('a').attr("href");
+            menus = menus.replace("#","");
+            fnLoadMenus(menus);
+            $('footer.mobileMenu').removeClass("menuOpen");
+            
+        });
+        
         window.setInterval(function(){
                var decWidth = Math.random() * winWidth;
               
@@ -70,11 +85,29 @@ function fnLoadMenus(loc){
             case "Photos":
                 menu ="photos";
                 break;
+            case "Events":
+                menu ="events";
+                break;    
         }
+        $('.desktopMenu li').removeClass("current");
+    $('.desktopMenu li a i').attr("class","fa fa-heart-o ");
+    
+    
+        $('.desktopMenu li.'+menu+"Li").addClass("current").find("i").removeClass("fa-heart-o").addClass("fa-heart");
+    
+        changeMenuMob(menu,loc);
+    if(menu != "videos")
+        videoOff();
+    else{
+        videoOn();
+    }
         if(!loadedMenu[menu]){
             loadedMenu[menu] = 1;
             $.get(menu+".html", function(data){
-                $('#container').html(data);
+                $('.page').hide();
+                
+            
+                $('#container').append(data);
                 $('.rel').width($('.rel').height());
                 if(menu === "story"){
                     //alert($('#story .current img ').get(0))
@@ -90,6 +123,31 @@ function fnLoadMenus(loc){
                 }
             });
         }    
+    
+        else{
+            $('.page').hide();
+            $('#'+menu).show();
+        }
         
         
+}
+
+function changeMenuMob(menu,loc){
+    //alert(menu+"  "+loc);
+    $('.mobileMenu .mobileCurrMenu').html(loc);
+    $('.mobileMenu li').removeClass("current");
+    $('.mobileMenu li.'+menu+"Li").addClass("current");
+}
+
+function videoOff(){
+    if($('#bgvid').length > 0){
+        $('#bgvid').remove();
+        videoRm = true;
+    }
+}
+function videoOn(){
+    if(videoRm){
+        $('#videos').html('<object id="bgvid"/>')
+        $('#bgvid').attr("data","https://www.youtube.com/v/SQiZM6N1vNA?autoplay=1&controls=0&loop=1&playlist=SQiZM6N1vNA");
+    }
 }
